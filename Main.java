0237ebb3424c08585f1c0eded884046d1c02dfe3
BechtelLo25 <91638125+BechtelLo25@users.noreply.github.com>
2023-11-30 20:12:42 -0500

    Update: Make the app more visually apealing

--- Main.java
+++ Main.java
@@ -1,9 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import javax.swing.border.LineBorder;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GradientPaint;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.geom.Ellipse2D;
 
 public class Main {
 
@@ -13,21 +21,38 @@ public class Main {
     static SkyblockTracker skyblockTracker = new SkyblockTracker();
     public static void main(String[] args) {
 
-        createAndShowGUI();
+        createAndShowJframe();
 
     }
 
-    private static void createAndShowGUI() {
+    private static void createAndShowJframe() {
         JFrame frame = new JFrame("Logan's Auction Tracker");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(700, 400);
+        frame.setSize(1000, 600);
 
-        JPanel panel = new JPanel();
+        JPanel panel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+
+                // Create a gradient from gray to black
+                GradientPaint gradient = new GradientPaint(100, 100, new Color(70, 70, 70), getWidth(), getHeight(), Color.BLACK);
+
+                // Set the gradient paint
+                ((Graphics2D) g).setPaint(gradient);
+
+                // Fill the panel with the gradient
+                g.fillRect(0, 0, getWidth(), getHeight());
+            }
+        };;
 
         String[] items = {"Select Player", "LoganMC618", "Ktf_Party"};
 
         JComboBox<String> dropdown = new JComboBox<>(items);
         JTextArea textArea = new JTextArea();
+        textArea.setBackground(Color.DARK_GRAY);
+        textArea.setForeground(Color.WHITE);
+        textArea.append("Welcome to Logan's skyblock tracker! Select a player to track.");
         
         dropdown.addActionListener(new ActionListener() {
             @Override
@@ -36,22 +61,32 @@ public class Main {
                 if ("LoganMC618".equals(dropdown.getSelectedItem())) {
                     textArea.setText("");
                     skyblockTracker.playerID = loganMC618PlayerUuid;
-                    textArea.append("Logan selected!\nPlayer Uuid: " + skyblockTracker.playerID + "\n");
+                    textArea.append("Logan selected!\n\nPlayer Uuid: " + skyblockTracker.playerID + "\n\n");
+                    textArea.append("Ten Mose Recent Auctions: \n\n");
+                    textArea.append(skyblockTracker.getTenAuctions());
+                }
+                else if ("Ktf_Party".equals(dropdown.getSelectedItem())) {
+                    textArea.setText("");
+                    skyblockTracker.playerID = ktf_PartyPlayerUuid;
+                    textArea.append("Zack selected!\n\nPlayer Uuid: " + skyblockTracker.playerID + "\n\n");
+                    textArea.append("Ten Mose Recent Auctions: \n\n");
                     textArea.append(skyblockTracker.getTenAuctions());
                 }
                 else {
-                    textArea.setText("");
-                    skyblockTracker.playerID = ktf_PartyPlayerUuid;
-                    textArea.append("Zack selected!\nPlayer Uuid: " + skyblockTracker.playerID + "\n");
-                    textArea.append(skyblockTracker.getTenAuctions());
+                    textArea.setText("No one is selected dummy");
                 }
             }
         });
 
+        Font customFont = new Font("Trebuchet MS", Font.PLAIN, 14); // You can customize the font here
+        textArea.setFont(customFont);
+        textArea.setBorder(new LineBorder(Color.WHITE, 2));
+        
         panel.add(dropdown);
         panel.add(textArea);
         frame.setContentPane(panel);
         frame.setLocationRelativeTo(null);
+        frame.getContentPane().setBackground(new Color(40, 40, 40));
         frame.setVisible(true);
     }
 }
