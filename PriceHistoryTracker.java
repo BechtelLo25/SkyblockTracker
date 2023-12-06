581427e7ab4afcef41fbf5e2c4e736a0ed4d96c7
Logan Bechtel <bechtello25@spaldinghs.org>
2023-12-06 13:51:06 -0500

    Feat: Finish price history tracker

--- PriceHistoryTracker.java
+++ PriceHistoryTracker.java
@@ -39,7 +39,7 @@ public class PriceHistoryTracker {
         String dayApiUrl = "https://sky.coflnet.com/api/item/price/" + getItemID(itemID)+ "/history/day?=string&=string&=string";
         String weekApiUrl = "https://sky.coflnet.com/api/item/price/" + getItemID(itemID)+ "/history/week?=string&=string&=string";
         String monthApiUrl = "https://sky.coflnet.com/api/item/price/" + getItemID(itemID)+ "/history/month?=string&=string&=string";
-        String yearApiUrl = "https://sky.coflnet.com/api/item/price/" + getItemID(itemID)+ "/history/year?=string&=string&=string";
+        String allApiUrl = "https://sky.coflnet.com/api/item/price/" + getItemID(itemID)+ "/history/full?=string&=string&=string";
 
         try {
             HttpClient client = HttpClient.newHttpClient();
@@ -68,8 +68,8 @@ public class PriceHistoryTracker {
                     .GET()
                     .build();
 
-            HttpRequest yearRequest = HttpRequest.newBuilder()
-                    .uri(URI.create(yearApiUrl))
+            HttpRequest allRequest = HttpRequest.newBuilder()
+                    .uri(URI.create(allApiUrl))
                     .header("accept", "text/plain")
                     .GET()
                     .build();
@@ -78,11 +78,15 @@ public class PriceHistoryTracker {
             HttpResponse<String> dayResponse = client.send(dayRequest, HttpResponse.BodyHandlers.ofString());
             HttpResponse<String> weekResponse = client.send(weekRequest, HttpResponse.BodyHandlers.ofString());
             HttpResponse<String> monthResponse = client.send(monthRequest, HttpResponse.BodyHandlers.ofString());
-            HttpResponse<String> yearResponse = client.send(yearRequest, HttpResponse.BodyHandlers.ofString());
+            HttpResponse<String> yearResponse = client.send(allRequest, HttpResponse.BodyHandlers.ofString());
 
-            String buyAndSellPriceCurrent = "Current Sell Price: " + currentResponse.body().substring(currentResponse.body().indexOf("sell") + 6, currentResponse.body().indexOf("buy") -2) + "\n" + "Current Buy Price: " + currentResponse.body().substring(currentResponse.body().indexOf("buy") + 6, currentResponse.body().indexOf("avail") -2) + "\n\n";
+            String buyAndSellPriceCurrent = "Current Sell Price: " + CommaAdder.addCommas(currentResponse.body().substring(currentResponse.body().indexOf("sell") + 6, currentResponse.body().indexOf("buy") - 4)) + "\n\n";
+            String buyAndSellPriceDay = "One Day Ago: " + CommaAdder.addCommas(dayResponse.body().substring(dayResponse.body().indexOf("avg") + 5, dayResponse.body().indexOf("volume") - 4)) + "\n\n";
+            String buyAndSellPriceWeek = "One Week Ago: " + CommaAdder.addCommas(weekResponse.body().substring(weekResponse.body().indexOf("avg") + 5, weekResponse.body().indexOf("volume") - 4)) + "\n\n";
+            String buyAndSellPriceMonth = "One Month Ago: " + CommaAdder.addCommas(monthResponse.body().substring(monthResponse.body().indexOf("avg") + 5, monthResponse.body().indexOf("volume") - 4)) + "\n\n";
+            String buyAndSellPriceYear = "One Year Ago: " + CommaAdder.addCommas(yearResponse.body().substring(yearResponse.body().indexOf("avg") + 5, yearResponse.body().indexOf("volume") - 4)) + "\n\n";
 
-            return buyAndSellPriceCurrent;
+            return buyAndSellPriceCurrent + buyAndSellPriceDay + buyAndSellPriceWeek + buyAndSellPriceMonth + buyAndSellPriceYear;
         } catch (Exception e) {
             e.printStackTrace();
         } 
